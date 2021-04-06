d66b6a7a5bb4cfecf2f57b9f087eafd7c9d8ae69
Prateek Jain <jain22719@gmail.com>
2021-04-06 21:34:26 +0530

    User Routes

---
+++ route/userRoute.js
@@ -0,0 +1,70 @@
+const http = require('http')
+const url = require('url')
+const { registerUser, updateUser, getUser, getUsers, deleteUser } = require('../service/userService')
+
+
+var server = http.createServer((req, res) => {
+    var Path = url.parse(req.url, true)
+    var page = Path.pathname
+
+    if (page === '/') {
+
+        res.end('Home');
+
+    } else if (page === '/Add') {
+
+        let User = {
+            firstname: String(Path.query.firstname),
+            lastname: String(Path.query.lastname),
+            age: Number(Path.query.age),
+            gender: String(Path.query.gender),
+            email: String(Path.query.email)
+        }
+
+        let data = registerUser(User);
+        
+        data(User).then((data)=>{
+            res.writeHead(200, { contentType: 'text' })
+            res.end(JSON.stringify(data))
+        });
+
+    } else if (page === '/Remove') {
+
+        let data =deleteUser({ email: Path.query.email });
+
+        data().then((data)=>{
+            res.writeHead(200, { contentType: 'text' })
+            res.end(JSON.stringify(data))
+        });
+
+    } else if (page === '/Update') {
+
+        let data = updateUser({ email: Path.query.email }, { age: Path.query.age });
+
+        data().then((data)=>{
+            res.writeHead(200, { contentType: 'text' })
+            res.end(JSON.stringify(data))
+        });
+
+    } else if (page === '/Read') {
+
+        let data = getUser({ email: Path.query.email});
+
+        data().then((data)=>{
+            res.writeHead(200, { contentType: 'text' })
+            res.end(JSON.stringify(data))
+        })
+
+    } else if (page === '/Reads') {
+
+        let data = getUsers({ email: Path.query.emails });
+        
+        data().then((data)=>{
+            res.writeHead(200, { contentType: 'text' })
+            res.end(JSON.stringify(data))
+        })
+
+    }
+})
+
+module.exports=server;
