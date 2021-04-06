95716cf495328e62ff860a71cef0da701bc1186c
Prateek Jain <jain22719@gmail.com>
2021-04-06 21:21:25 +0530

    server creation

---
+++ server.js
@@ -0,0 +1,27 @@
+const mong = require('mongoose')
+var http = require('http');
+
+mong.connect('mongodb://127.0.0.1:27017/User', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+
+let startNodeserver = async () => {
+    // express startup.
+    server = await require('');
+
+    return new Promise((resolve, reject) => {
+        server.listen(4000, (err) => {
+            if (err) reject(err);
+            resolve();
+        });
+    });
+};
+
+startNodeserver()
+    .then(() => {
+        console.log('Node server running on 4000');
+    }).catch((err) => {
+        console.log('Error in starting server', err);
+        process.exit(1);
+    });
